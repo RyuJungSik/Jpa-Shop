f4ed1e67940ebb4e1f2f985db0607063caf24e4c
RyuJungSik <xvbc123@gmail.com>
2023-05-06 20:30:01 +0900

    Feat : 상품 엔티티에 비즈니스 로직 추가

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.domain.item;
 
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -11,7 +12,8 @@ import java.util.List;
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn(name = "dtype")
-@Getter @Setter
+@Getter
+@Setter
 public abstract class Item {
 
 @Id
@@ -25,5 +27,22 @@ private int stockQuantity;
 
 @ManyToMany(mappedBy = "items")
 private List<Category> categories = new ArrayList<>();
+
+
+//==비즈니 로직==/
+
+//stock 증가
+public void addStock(int quantity) {
+    this.stockQuantity += quantity;
+}
+
+//stock 증가
+public void removeStock(int quantity) throws NotEnoughStockException {
+    int restStock = this.stockQuantity - quantity;
+    if (restStock < 0) {
+        throw new NotEnoughStockException("need moew stock");
+    }
+    this.stockQuantity = restStock;
+}
 
 }
