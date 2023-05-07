674791e294712b355aa9a88b5df948b60ab73691
RyuJungSik <xvbc123@gmail.com>
2023-05-08 00:11:01 +0900

    Feat : 홈 화면 추가

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
+import java.util.List;
 
 @Repository
 @RequiredArgsConstructor
@@ -19,9 +20,5 @@ public void save(Order order) {
 public Order findOne(Long id){
     return em.find(Order.class, id);
 }
-
-//public List<Order> findAll(OrderSearch orderSearch){
-//
-//}
 
 }
